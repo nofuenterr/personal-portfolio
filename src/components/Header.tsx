175e46e4c1cf9a2307979986891baaa9ec50998b
RR <rrn091301@gmail.com>
2026-01-12 20:51:26 +0800

    feat: add header content and styles with sun and moon icon for theme toggle ui, and icon component prop type

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,47 @@
+import { NavLink, Link } from 'react-router-dom';
+import { useThemeStore } from '../stores/themeStore';
+import Sun from './icons/Sun';
+import Moon from './icons/Moon';
+
 export default function Header() {
-	return <header>Header</header>;
+	const { dark, switchTheme } = useThemeStore();
+
+	return (
+		<header className="mb-10 flex items-center justify-between gap-4">
+			<div className="font-brand text-lg font-bold -tracking-widest">
+				<Link to="/">RR</Link>
+			</div>
+
+			<div className="flex items-center gap-4">
+				<nav className="lowercase">
+					<ul className="flex items-center gap-3">
+						<li className="hover:text-muted-foreground-light dark:hover:text-muted-foreground-dark transition-colors">
+							<NavLink to="/">Home</NavLink>
+						</li>
+						<li className="hover:text-muted-foreground-light dark:hover:text-muted-foreground-dark transition-colors">
+							<NavLink to="/projects">Projects</NavLink>
+						</li>
+						<li className="hover:text-muted-foreground-light dark:hover:text-muted-foreground-dark transition-colors">
+							<a href="mailto:rrn091301@gmail.com">Contact</a>
+						</li>
+					</ul>
+				</nav>
+
+				<button
+					onClick={() => switchTheme()}
+					className="cursor-pointer transition-transform"
+				>
+					{dark ? (
+						<p>
+							<Moon className="hover:scale-110" />
+						</p>
+					) : (
+						<p>
+							<Sun className="hover:scale-110" />
+						</p>
+					)}
+				</button>
+			</div>
+		</header>
+	);
 }
---
+++ src/components/icons/Sun.tsx
@@ -0,0 +1,22 @@
+import type { IconComponentProps } from '../../types/iconComponentProps';
+
+export default function Sun(props: IconComponentProps) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width={24}
+			height={24}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth={2}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			className="lucide lucide-sun-icon lucide-sun"
+			{...props}
+		>
+			<circle cx={12} cy={12} r={4} />
+			<path d="M12 2v2M12 20v2M4.93 4.93l1.41 1.41M17.66 17.66l1.41 1.41M2 12h2M20 12h2M6.34 17.66l-1.41 1.41M19.07 4.93l-1.41 1.41" />
+		</svg>
+	);
+}
